c5dae5213e4461f395e18bcf86dd6d38fa9d4a51
nefrob <nefrob.skyguy@gmail.com>
2024-01-14 12:42:29 -0700

    WIP changes on keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,72 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap.set
+
+-- escape in insert mode
+keymap('i', 'jj', '<Esc>', {})
+
+
+-- local M = {}
+
+-- M.saving = {
+--     i = {
+--         -- save
+--         ["<leader>w"] = { "<cmd> w <CR>", "save buffer" },
+--     },
+--     n = {
+--         -- save
+--         ["<leader>w"] = { "<cmd> w <CR>", "save buffer" },
+--         -- save and quit
+--         ["<leader>wq"] = { "<cmd> wq <CR>", "save and close buffer" },
+--     },
+-- }
+
+-- M.tabbing = {
+--     i = {
+--         -- outdent
+--         ["<S-Tab>"] = { "<C-d>", "outdent" },
+--     },
+--     n = {
+--         -- indent and outdent
+--         ["<C-]>"] = { ">>", "indent" },
+--         ["<C-[>"] = { "<<", "outdent" },
+--     },
+--     v = {
+--         -- indent and outdent
+--         ["<C-]>"] = { ">gv", "indent" },
+--         ["<C-[>"] = { "<gv", "outdent" },
+--     },
+-- }
+
+-- M.movement = {
+--     n = {
+--         -- line movement on wrapped lines
+--         -- first two are already done by nvchad
+--         -- ["<silent> j"] = { "gj", "down" },
+--         -- ["<silent> k"] = { "gk", "up" },
+--         ["<silent> h"] = { "g^", "start" },
+--         ["<silent> l"] = { "g$", "end" },
+--         ["<leader>e"] = { "$", "end" },
+--         ["<leader>ye"] = { "y$", "copy though end" },
+--         ["<leader>de"] = { "d$", "delete through end" },
+--         ["<leader>sf"] = { "^", "start" },
+--     },
+-- }
+
+-- M.scroll = {
+--     n = {
+--         ["<S-j>"] = { "<C-e>", "scroll down" },
+--         ["<S-k>"] = { "<C-y>", "scroll up" },
+--     },
+-- }
+
+-- M.scroll = {
+--     n = {
+--         ["<S-j>"] = { "<C-e>", "scroll down" },
+--         ["<S-k>"] = { "<C-y>", "scroll up" },
+--     },
+-- }
+
+-- return M
